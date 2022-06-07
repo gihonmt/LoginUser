e136b81a7c37f79fb2872be66b31c4af0bdfc13f
gihonmt <49367135+gihonmt@users.noreply.github.com>
2022-06-07 16:03:53 +0700

    Update konek_database.php

--- konek_database.php
+++ konek_database.php
@@ -1,7 +1,7 @@
 <?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$database = "database_login";
+$server = "sql5.freesqldatabase.com";
+$user = "sql5498213";
+$password = "cc3SS9I6Wf";
+$database = "sql5498213";
 
 $konek_database = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($konek_database));
